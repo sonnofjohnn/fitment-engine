8777ef226d5318673a86299825ace73a3678e8f0
Johnson Kwan <jkwan@candywarehouse.com>
2026-03-22 23:11:48 -0700

    Update to new Shopify app credentials

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -15,6 +15,12 @@ export default function App() {
 
   return (
     <AppProvider embedded apiKey={apiKey}>
+      <s-app-nav>
+        <s-link href="/app">Dashboard</s-link>
+        <s-link href="/app/fitment-setup">Attribute Setup</s-link>
+        <s-link href="/app/vehicle-data">Attribute Data</s-link>
+        <s-link href="/app/fitment-assign">Attribute Assignment</s-link>
+      </s-app-nav>
       <Outlet />
     </AppProvider>
   );
